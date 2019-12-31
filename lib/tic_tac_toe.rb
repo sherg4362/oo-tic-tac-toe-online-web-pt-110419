19adb4d18029487e15be0b5e5dc4f46363c21fc2
Sheriefs Gamble <sgamble284@gmail.com>
2019-12-31 10:59:18 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,48 @@
+
+class TicTacToe
+  WIN_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2]
+    ]
+  
+  def initialize
+    @board = Array.new(9, " ")
+  end
+  
+  
+  def display_board
+    
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "------------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "------------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+  
+  def input_to_index(user_input)
+    user_input.to_i - 1
+  end
+  
+  def move(index, token = "X")
+    @board[index] = token
+  end
+  
+  def position_taken?(index)
+    if @board[index] == " " || @board[index] == "" || @board[index] == nil
+      return false
+    else
+      return true
+    end
+  end
+  
+  def valid_move?(position_to_check)
+    
+  end
+  
+end
